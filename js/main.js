0abf1cddff7a2a1212dde74dd00b85f579421844
Samuel Cheney <cheneysamuel@gmail.com>
2020-02-17 12:09:34 -0500

    Populated with empty functions
    Did not populate all functions.  Simple descriptions of the functions are given, but no pseudocode or in-depth descriptions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,96 @@
 
+
+
+function xmlLoadFile(){}
+
+
+function validateQAobject(QAobject){
+  // ensure that the QAobject has everything required to begin a game.
+  // the return value should reflect either that a game is possible
+  // or returns what is needed.
+
+}
+
+
+function populateGameGrid(QAobject){}
+
+
+function gridObjectAction(actionType){
+  // remember "this"
+  // use the "actionType" along with the gameMode to determine action taken
+
+}
+
+
+function overlayClick(){
+  // use the gameMode to determine action taken
+
+
+}
+
+
+function gameModeChange(newGameMode){
+  // change the current game mode.  still deciding all the game modes available to use
+  // such as inGame, inGameShowingAnswer, inGameShowingQuestion, inGameShowingFinalQA,
+  // editingGame, editingGame_changesMade, newGame_changesMade, etc.
+  
+  // used to change the global variable controlling the gameMode
+  
+}
+
+
+function showAdminWindow(){
+  // is there a way to determine whether or not a dual screen is available?
+  // if so, warn user if not available.  else open the admin window
+
+}
+
+
+function onAdminWindowClose(){
+  // do these things when the admin window is closed
+
+}
+
+
+// administrator functions:
+
+function editQAobject(ob){
+  // the ob should include attributes such as "type", "value", etc.
+
+}
+
+
+function importGameFile(){
+  // import the xml game file
+
+}
+
+
+function exportGameFile(gameObject){
+  // export the xml game file
+  
+}
+
+
+function createNewGame(){
+  // create a new game (not yet xml)
+  
+}
+
+
+function editColumn(col, action){
+  // 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
